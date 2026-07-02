d09400d82e67a8ae1399e975a687f3a453133f6b
Muh Amirul Rizal <156772298+rizalckr@users.noreply.github.com>
2026-07-02 12:39:42 +0700

    Remove base path from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/nota_onlen',
   server: {
     host: '0.0.0.0', // This makes the server accessible externally
   },
